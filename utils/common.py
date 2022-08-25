33e56cb66e91d3ced314f45217368b26ffdc9820
DESKTOP-MMLAB-O\JinhaSong <jinhasong@sogang.ac.kr>
2022-08-25 19:49:36 +0900

    불필요한 함수 제거

--- utils/common.py
+++ utils/common.py
@@ -1,12 +1,2 @@
 from utils import Logging
 
-def get_task(str_task):
-    task_category = ["rename", "align"]
-    tasks = str_task.split(",")
-    flag = False
-    for task in tasks:
-        for tc in task_category:
-            if task == tc:
-                flag = True
-    assert flag, Logging.e("\"{}\" task is incorrect.".format(task))
-    return tasks
